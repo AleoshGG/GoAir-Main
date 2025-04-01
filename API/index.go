d3c632b73bd9e71dcb7381ae0ceded93bfaacf44
Alexis Guzman <spartanthelexvro@gmail.com>
2025-04-01 17:15:22 -0600

    Añadiendo cors

--- API/index.go
+++ API/index.go
@@ -5,6 +5,7 @@ import (
 	"API/sensors/infrastructure"
 	"API/sensors/infrastructure/routes"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -12,10 +13,12 @@ import (
 func main() {
 	godotenv.Load()
 	conn.Migration()
-
+	
 
 	infrastructure.GoDependences()
 	r := gin.Default()
+	r.Use(cors.Default())
+	
 	routes.RegisterRouter(r)
 	r.Run()
 }
